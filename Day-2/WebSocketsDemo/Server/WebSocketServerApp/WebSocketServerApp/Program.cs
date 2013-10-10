d821b48facc2faab77ee90e55edacd392ab7facb
the-javascript-school <tkmagesh@craftsmanfactory.com>
2013-10-10 11:35:50 -0700

    WebSockets and Geolocation Demo

---
+++ Day-2/WebSocketsDemo/Server/WebSocketServerApp/WebSocketServerApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSocketServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var server = new WebSocketServer("ws://localhost:9090/SocketServer");
+            List<IWebSocketConnection> conns = new List<IWebSocketConnection>();
+            server.Start(con => {
+                con.OnOpen = () =>
+                {
+                    conns.Add(con);
+                    Console.WriteLine("A new connection is opened..");
+                };
+                con.OnClose = () =>
+                {
+                    conns.Remove(con); ;
+                    Console.WriteLine("An existing connection is closed");
+                };
+                con.OnMessage = (m) => {
+                    Console.WriteLine("Message received - {0}",m);
+                    foreach (var connection in conns)
+                    {
+                        if (connection != con)
+                            connection.Send(m);
+                    }
+                };
+
+            });
+            Console.WriteLine("Enter any text to send to client OR 'exit' to shutdown");
+            var input = string.Empty;
+            while ((input = Console.ReadLine()).ToUpper() != "EXIT") {
+                foreach (var connection in conns)
+                {
+                    connection.Send(input);
+                }
+            } 
+        }
+
+        
+    }
+}
